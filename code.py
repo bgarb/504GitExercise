1e133edb5ef30d8525dd4fce3fc6ce2d349b4224
Bailey Garb <bgarb@umich.edu>
2022-08-24 10:06:56 -0400

    Updates function names

--- code.py
+++ code.py
@@ -7,8 +7,8 @@ def function1(a):
             b[c] += 1
     return b
 
-def function2(a):
-    print('freqs')
+def cal_frequecies(a):
+    print('freqs') 
     total = float(sum([a[b] for b in a.keys()]))
     for b in a.keys():
         print(b + ':' + str(a[b]/total))
